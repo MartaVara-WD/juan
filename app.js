7c3cba2c1e78b3b3406e8905128299855e5e11df
Marta García Abad <65690300+MartaVara-WD@users.noreply.github.com>
2022-02-06 00:00:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const btnMenuAbrir = document.querySelectorAll(".boton-polilla");
 const btnMenuCerrar = document.querySelector("#btn-cerrar-menu");
 const btnBloqueContacto = document.querySelector("#btn-contacto");
 const btnBloqueArt = document.querySelector("#btn-art");
+const btnBloqueShop = document.querySelector("#btn-shop");
+const btnBloqueInstagram = document.querySelector("#btn-instagram");
+const btnsTodos = document.querySelectorAll(".botones-portada");
+
+
 
 
 //--Boton portada
@@ -40,10 +45,19 @@ btnMenuCerrar.addEventListener('click', function () {
     console.log("btn cerrar");
     menu.classList.add("oculto");
 });
+//-- Funcion quitar color a todos los botones
+function borrarColorBotones() {
+    for (var i = 0; i < btnsTodos.length; i++) {
+        btnsTodos[i].style.color = "#070707"
+    }
+
+}
 
 //--Boton bloque contacto
 btnBloqueContacto.addEventListener('click', function () {
     console.log("contacto");
+    borrarColorBotones()
+    btnBloqueContacto.style.color = "#b33232"
     bloqueContacto.classList.remove("oculto");
     menu.classList.add("oculto");
     bloqueArt.classList.add("oculto");
@@ -52,9 +66,33 @@ btnBloqueContacto.addEventListener('click', function () {
 //--Boton bloque art
 btnBloqueArt.addEventListener('click', function () {
     console.log("art");
+    borrarColorBotones()
+    btnBloqueArt.style.color = "#b33232"
     bloqueArt.classList.remove("oculto");
     menu.classList.add("oculto");
     bloqueContacto.classList.add("oculto");
+
 });
+
+//--Boton bloque shop
+btnBloqueShop.addEventListener('click', function () {
+    console.log("shop");
+    borrarColorBotones()
+    btnBloqueShop.style.color = "#b33232"
+});
+
+//--Boton bloque instagram
+btnBloqueInstagram.addEventListener('click', function () {
+    console.log("btnBloqueInstagram");
+    borrarColorBotones()
+    btnBloqueInstagram.style.color = "#b33232"
+});
+
+
+
+
+//--cuando inicia el codigo 
+//-- --art aparece en rojo
+btnBloqueArt.style.color = "#b33232"
 
 
